2409c12f966912d59435b9998bf48fd48cff39b2
周竑亦 <chou960916@gmail.com>
2024-01-29 19:59:48 +0800

    initial commit

---
+++ d006.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h> // This is the only line you need to copy to your code.
+using namespace std; 
+#define penguin ios::sync_with_stdio(0);cin.tie(0);
+#define all(_) _.begin(),_.end()
+#define pr(_) cout << fixed << setprecision(_)
+typedef long long ll;
+typedef long double ld;
+
+int main(void) {
+    penguin
+    return 0;
+}
